db11e294754cf8ecb1c0e57d7c729c5fdb36d8e5
Lucas Lopes <lfalcaolopes@gmail.com>
2024-12-21 19:38:29 -0300

    feat: add defaultModalities

---
+++ src/helpers/defaultModalities.ts
@@ -0,0 +1,28 @@
+import Basketball from '../../assets/icons/Basketball.png';
+import BeachTennisBall from '../../assets/icons/BeachTennisBall.png';
+import SoccerBall from '../../assets/icons/SoccerBall.png';
+import TennisBall from '../../assets/icons/TennisBall.png';
+import Volleyball from '../../assets/icons/Volleyball.png';
+
+export const DefaultModalities = {
+	soccer: {
+		name: 'Futebol',
+		icon: SoccerBall
+	},
+	basketball: {
+		name: 'Basquete',
+		icon: Basketball
+	},
+	beachTennis: {
+		name: 'Beach Tennis',
+		icon: BeachTennisBall
+	},
+	tennis: {
+		name: 'Tênis',
+		icon: TennisBall
+	},
+	volleyball: {
+		name: 'Vôlei',
+		icon: Volleyball
+	}
+};
