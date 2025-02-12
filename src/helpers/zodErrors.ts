2ad279c6816a6afed38c94bb2722ebece649a0e6
Lucas Lopes <lfalcaolopes@gmail.com>
2025-02-12 01:44:31 -0300

    feat: add requiredField error message in zodErrors

--- src/helpers/zodErrors.ts
+++ src/helpers/zodErrors.ts
@@ -1,4 +1,5 @@
 export const zodErrors = {
+	requiredField: 'Campo obrigatório',
 	emptyString: (fieldName: string) => `O campo de ${fieldName} não pode estar vazio`,
 	invalidEmail: 'Email invalido',
 	passwordMismatch: 'As senhas não coincidem'
